5c790609256b1e989757369b99a77a15b5bf9c28
arpithasureshappa <arpitha.sureshappa@tarento.com>
2025-09-15 11:35:36 +0530

    added test cases to increase code coverage

--- src/test/java/com/igot/cb/util/CbServerPropertiesTest.java
+++ src/test/java/com/igot/cb/util/CbServerPropertiesTest.java
@@ -11,16 +11,19 @@ class CbServerPropertiesTest {
     @Test
     void testRecentSearchesLimitField() throws Exception {
         CbServerProperties props = new CbServerProperties();
-
-        // Use reflection to set the private field
         Field field = CbServerProperties.class.getDeclaredField("recentSearchesLimit");
         field.setAccessible(true);
         field.set(props, 15);
-
-        // Read back the field value
         Integer value = (Integer) field.get(props);
-
         assertEquals(15, value);
+    }
+
+    @Test
+    void testGetterSetter() {
+        CbServerProperties props = new CbServerProperties();
+        props.setRecentSearchesLimit(25);
+        Integer value = props.getRecentSearchesLimit();
+        assertEquals(25, value);
     }
 }
 
