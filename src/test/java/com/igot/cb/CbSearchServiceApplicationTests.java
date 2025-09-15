b006f38d34a217a59b9d5edc535720f7356c96de
arpithasureshappa <arpitha.sureshappa@tarento.com>
2025-09-15 12:37:56 +0530

    added test cases to increase code coverage

--- src/test/java/com/igot/cb/CbSearchServiceApplicationTests.java
+++ src/test/java/com/igot/cb/CbSearchServiceApplicationTests.java
@@ -1,11 +1,36 @@
 package com.igot.cb;
 
+import com.igot.cb.search.service.SearchService;
+import com.igot.cb.util.CbServerProperties;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class CbSearchServiceApplicationTests {
+    @Autowired
+    private SearchService searchService;
 
+     @MockBean
+     private CbServerProperties cbServerProperties;
+
+    @Test
+    void contextLoads() {
+        // Just verifies Spring Boot context loads successfully
+    }
+
+    @Test
+    void searchServiceBeanLoads() {
+        assertThat(searchService).isNotNull();
+    }
+
+    @Test
+    void cbServerPropertiesLoads() {
+        assertThat(cbServerProperties).isNotNull();
+    }
 }
